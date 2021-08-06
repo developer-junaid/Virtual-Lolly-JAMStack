9ba148bea0ffe7c21a326e5847063954976cdbf5
developer-junaid <qjunaid623@gmail.com>
2021-08-06 19:55:51 +0500

    added footer, NEXT: Functionality

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -45,6 +45,18 @@ export default function Landing() {
       >
         Get Started
       </Link>
+
+      <footer className="border-t bottom-0 mt-6 p-4 ">
+        <h2 className="text-white">
+          Developed by{" "}
+          <a
+            href="https://github.com/developer-junaid"
+            className="text-blue-400 hover:text-blue-600 underline cursor-pointer"
+          >
+            Junaid
+          </a>
+        </h2>
+      </footer>
     </div>
   )
 }
