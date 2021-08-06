95a506e4d524a561024dbe431f44386994215cc7
developer-junaid <qjunaid623@gmail.com>
2021-08-06 07:58:54 +0500

    created lolly svg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
+// Components
+import Lolly from "./components/Lolly"
+
+// Home
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Lolly />
 }
