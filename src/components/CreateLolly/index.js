629531fafb1c556516d68a20086aedca7c87d842
developer-junaid <qjunaid623@gmail.com>
2021-08-11 08:18:18 +0500

    Completed !!

--- src/components/CreateLolly/index.js
+++ src/components/CreateLolly/index.js
@@ -55,6 +55,7 @@ const CreateLolly = () => {
 
         // Post Data
         await createLolly(to, from, message, topColor, middleColor, bottomColor)
+        resetForm()
       }}
     >
       {({
@@ -62,7 +63,6 @@ const CreateLolly = () => {
         errors,
         touched,
         handleChange,
-        handleBlur,
         handleSubmit,
         isSubmitting,
       }) => (
@@ -110,37 +110,37 @@ const CreateLolly = () => {
 
               <div className="mt-8 flex flex-col">
                 <input
-                  className="py-2 px-4  focus:ring focus:border-blue-300 outline-none my-2 text-lg xl:text-xl"
+                  className="py-2 px-4  focus:ring focus:border-blue-300 outline-none mt-4 mb-1 text-lg xl:text-xl"
                   type="text"
                   name="to"
                   placeholder="To*"
                   onChange={handleChange}
                   value={values.to || ""}
                 />
-                <span className="text-white text-2xl">
+                <span className="text-red-600">
                   {errors.to && touched.to && errors.to}
                 </span>
 
                 <input
-                  className="py-2 px-4 focus:ring focus:border-blue-300 outline-none  xl:text-xl my-2 text-lg"
+                  className="py-2 px-4 focus:ring focus:border-blue-300 outline-none  xl:text-xl mt-4 mb-1  text-lg"
                   type="text"
                   name="message"
                   placeholder="Message*"
                   value={values.message || ""}
                   onChange={handleChange}
                 />
-                <span className="text-white text-2xl">
+                <span className="text-red-600">
                   {errors.message && touched.message && errors.message}
                 </span>
                 <input
-                  className="py-2 px-4 focus:ring focus:border-blue-300 xl:text-xl outline-none my-2 text-lg"
+                  className="py-2 px-4 focus:ring focus:border-blue-300 xl:text-xl outline-none mt-4 mb-1 text-lg"
                   type="text"
                   name="from"
                   placeholder="From*"
                   value={values.from || ""}
                   onChange={handleChange}
                 />
-                <span className="text-white text-2xl">
+                <span className="text-red-600">
                   {errors.from && touched.from && errors.from}
                 </span>
                 <button
