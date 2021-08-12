638974ca050782bb17620d7b620324e1aa372f59
developer-junaid <qjunaid623@gmail.com>
2021-08-12 07:46:46 +0500

    fixed footer bug

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="border-t w-full flex-none mt-auto p-4 ">
       <h2 className="text-white text-lg">
-        Developed by
+        Developed by&nbsp;
         <a
           href="https://github.com/developer-junaid"
           className="text-blue-400 hover:text-blue-600 underline cursor-pointer"
